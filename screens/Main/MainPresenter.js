96d51881d3a8c0d17fff006eff3d87b577e4d5ab
rovin0805 <qhdrn67@naver.com>
2021-06-04 18:47:14 +0900

    changed barcode icon to detail icon on main page

--- screens/Main/MainPresenter.js
+++ screens/Main/MainPresenter.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { SafeAreaView, Image, Dimensions } from "react-native";
+import { SafeAreaView, Image, Dimensions, View } from "react-native";
 import styled from "styled-components/native";
 import GlobalStyles from "../../components/GlobalStyles";
 import Background from "../../assets/background.jpg";
 import Barcode from "../../assets/barcode.png";
 import Question from "../../assets/question.png";
+import Detail from "../../assets/detail.jpg";
 import Carousel from "../../components/Carousel";
 
 const { width: deviceWidth, height: deviceHeight } = Dimensions.get("window");
@@ -17,6 +18,22 @@ const Wrapper = styled.View`
   position: absolute;
   top: 25%;
   z-index: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Bubble = styled.View`
+  padding: 10px;
+  background-color: #fff;
+  border-radius: 30px;
+`;
+
+const BubbleText = styled.Text`
+  color: #696969;
+  font-size: 12px;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 5px;
 `;
 
 const Button = styled.TouchableOpacity`
@@ -43,6 +60,25 @@ const QTouch = styled.TouchableOpacity`
   z-index: 1;
 `;
 
+const styles = {
+  triangle: {
+    width: 0,
+    height: 0,
+    backgroundColor: "transparent",
+    borderStyle: "solid",
+    borderTopWidth: 0,
+    borderRightWidth: 15,
+    borderBottomWidth: 30,
+    borderLeftWidth: 15,
+    borderTopColor: "transparent",
+    borderRightColor: "transparent",
+    borderBottomColor: "white",
+    borderLeftColor: "transparent",
+    transform: [{ rotate: "180deg" }],
+    marginBottom: 10,
+  },
+};
+
 export default ({ goToWhere, PopUpAlert }) => {
   return (
     <SafeAreaView style={GlobalStyles.AndroidSafeArea}>
@@ -51,8 +87,8 @@ export default ({ goToWhere, PopUpAlert }) => {
           <Image
             source={Question}
             style={{
-              width: 50,
-              height: 50,
+              width: 40,
+              height: 40,
             }}
           />
         </QTouch>
@@ -65,15 +101,22 @@ export default ({ goToWhere, PopUpAlert }) => {
           }}
         />
         <Wrapper>
-          <Image
-            source={Barcode}
-            style={{
-              width: 200,
-              height: 200,
-            }}
-          />
+          <Bubble>
+            <BubbleText>
+              제품의 바코드를 스캔하여 분리배출 정보를 얻을 수 있어요
+            </BubbleText>
+            <Image
+              source={Detail}
+              style={{
+                width: deviceWidth * 0.8,
+                height: 150,
+                resizeMode: "contain",
+              }}
+            />
+          </Bubble>
+          <View style={styles.triangle} />
           <Button onPress={() => goToWhere("Scan")} color={"#1e90ff"}>
-            <BtnText fontsize={20}>바코드 스캔</BtnText>
+            <BtnText fontsize={20}>바코드 스캔하기</BtnText>
           </Button>
           <Button onPress={() => goToWhere("Map")} color={"#27CD78"}>
             <BtnText fontsize={17}>내 주변 쓰레기통 찾기</BtnText>
